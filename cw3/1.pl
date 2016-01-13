e40fd026771d71b865f72b3fb44f4853ba155941
Jakub Skory <git@q84fh.net>
2016-01-13 11:59:29 +0100

    Zadanie pierwsze działa.

---
+++ cw3/1.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+#
+# Napisz program, który wczytuje listę liczb i dokonuje jej
+# sortowania numerycznego, wypisując rezultat tej operacji w
+# kolumnie wyrównanej do prawej strony. Utworzony program
+# wypróbuj, korzystając z przedstawionych poniżej danych
+# przykładowych:
+# 17 1000 04 1.50 3.14159 -10 1.5 4 2001 90210 666
+#
+use strict;
+use warnings;
+
+while(<>) {
+	print $_;
+}
